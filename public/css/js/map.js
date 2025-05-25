e25b5415497b295b111a6b246c9c1f3c1f9b28fb
Sanjit Singh <sanjit420sanju@gmail.com>
2025-05-26 00:10:56 +0530

    map fixing

--- public/css/js/map.js
+++ public/css/js/map.js
@@ -1,4 +1,4 @@
-
+//public folder js file has no access to process.env variables only access to ejs so ejs store it in js variables then we can access here
   mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
@@ -7,7 +7,7 @@
     });
     
     
-
+// we cant access coordinates here so write it in script then use like before 
     const marker = new mapboxgl.Marker({ color: "red" })
     .setLngLat(listing.geometry.coordinates)
     .setPopup(new mapboxgl.Popup({offset: 25})
