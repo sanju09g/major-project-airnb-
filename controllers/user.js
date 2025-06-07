19f718fa614f845835fe4805cf12e567d56516bf
Sanjit Singh <sanjit420sanju@gmail.com>
2025-06-07 12:34:37 +0530

    Validate Email Function added

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user.js");
+const valdiateEmail = require("../utils/validateEmail.js");
 
 module.exports.renderSignupForm =(req,res)=>{
     res.render("users/signup.ejs");
@@ -11,7 +12,18 @@ module.exports.renderLoginForm = (req,res)=>{
 module.exports.signup =async(req,res)=>{
     try{
         let {username,email,password} = req.body;
+
+        if (
+            !emailData ||
+            !emailData.is_valid_format.value ||
+            !emailData.is_smtp_valid.value
+          ) {
+            req.flash("error", "Invalid email address.");
+            return res.redirect("/signup");
+          }
         const newUser = new User({email,username});
+        const emailData = await valdiateEmail(email);
+       
          const registeredUser = await User.register(newUser,password);
          req.login(registeredUser,(err)=>{
             if(err){
---
+++ utils/validateEmail.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+
+
+const valdiateEmail = async(email)=>{
+const api_key = "f494cba3c19447f9a463a5f24e99e763";
+const url = `https://emailvalidation.abstractapi.com/v1/?api_key=${api_key}&email=${email}`;
+
+try {
+    const response = await axios.get(url);
+    console.log(response);
+    return response.data;
+  } catch (err) {
+    req.flash("error",err);
+    return null;
+  }
+
+}
+
+module.exports = valdiateEmail;
